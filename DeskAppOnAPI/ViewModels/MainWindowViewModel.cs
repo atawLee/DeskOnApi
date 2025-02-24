b14167a536f7f9d05f7e4f27e03a3510168095fe
jonghoon <jonghoon.1738@gmail.com>
2025-02-24 23:39:03 +0900

    Desktop on Api Sample

---
+++ DeskAppOnAPI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using API;
+using CommunityToolkit.Mvvm.Input;
+
+namespace DeskAppOnAPI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public ICommand ApiStartCommand { get; init; }
+
+    public MainWindowViewModel()
+    {
+        ApiStartCommand = new AsyncRelayCommand(StartApi);
+    }
+
+    private async Task StartApi()
+    {
+        ApiStarter starter = new();
+        await starter.StartApi();
+    }
+}
---
+++ DeskAppOnAPI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DeskAppOnAPI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
